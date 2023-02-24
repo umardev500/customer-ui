df3691d3dbf2888d01527a670c1cf7844641d306
umar <umar.dev500@gmail.com>
2023-02-24 14:58:49 +0700

    remove order text

--- components/molecules/productListing/ProductListing.tsx
+++ components/molecules/productListing/ProductListing.tsx
@@ -47,7 +47,6 @@ export const ProductListing: React.FC<Props> = ({ index, ...props }) => {
                         <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M6 12H18M12 18V6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
-                        <span className="mr-2 font-medium">Order</span>
                     </button>
                 </div>
                 {detailModal ? <ProductDetailModal {...props} setModalState={setDetailModal} /> : null}
