09db14c38f1c59cd21014e57363d1fb5198f93d2
umar <umar.dev500@gmail.com>
2023-02-27 14:08:46 +0700

    check for settlement is not undefined or not valid value

--- hooks/useExp.ts
+++ hooks/useExp.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react'
 import { getServerTime } from '../helpers'
 
-export const useExp = (setlement: number, dur: number): boolean => {
+export const useExp = (settlement: number, dur: number): boolean => {
     const [isExp, setIsExp] = useState<boolean>(false)
     const sec = dur * 24 * 60 * 60
-    const exp = setlement + sec
+    const exp = settlement + sec
 
     useEffect(() => {
         const calculate = async (): Promise<void> => {
             try {
                 const unixTime = await getServerTime()
-                setIsExp(unixTime > exp)
+                if (settlement > 0) setIsExp(unixTime > exp)
 
                 return await Promise.resolve()
             } catch (err) {
