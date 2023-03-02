84a84c76e40ef5fad046cb6324c589b0e5174de9
umar <umar.dev500@gmail.com>
2023-03-02 11:26:08 +0700

    samesite

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const baseURL = 'https://eb41-140-213-132-236.ap.ngrok.io'
+const baseURL = 'https://ba3d-140-213-142-232.ap.ngrok.io'
 // const baseURL = 'http://localhost:8000'
 
 const nextConfig = {
